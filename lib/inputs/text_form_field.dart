181c9ae0734e201cfc401f7f485c53af97181e34
remzigunes21 <remzi.gunescmp@gmail.com>
2021-09-21 21:54:07 +0300

    form işlemleri

--- lib/inputs/text_form_field.dart
+++ lib/inputs/text_form_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class TextFormFieldApp extends StatefulWidget {
   @override
@@ -6,6 +7,9 @@ class TextFormFieldApp extends StatefulWidget {
 }
 
 class _TextFormFieldAppState extends State<TextFormFieldApp> {
+  String _email = "", _password = "", _userName = "";
+
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +20,98 @@ class _TextFormFieldAppState extends State<TextFormFieldApp> {
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             child: Column(
               children: [
-                buildTextFormField("Name", "please enter name!"),
-                buildTextFormField("SurName", "please enter surname!"),
-                buildTextFormField("email", "please enter email!"),
-                buildTextFormField("password", "please enter password!"),
+                TextFormField(
+                  onSaved: (newValue) {
+                    _userName = newValue;
+                  },
+                  validator: (value) {
+                    if (value.length < 4) {
+                      return "Lütfen 4 ten fazla karakter gir";
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                    labelText: "User Name",
+                    hintText: "User Name enter",
+
+                    //contentPadding: EdgeInsets.all(8),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  onSaved: (newValue) {
+                    _email = newValue;
+                  },
+                  validator: (value) {
+                    final bool isValid = EmailValidator.validate(value);
+                    if (value.isEmpty) {
+                      return "email is not empty";
+                    } else if (!isValid) {
+                      return "Lütfen email şartlarını sağlayınız";
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Email enter",
+
+                    //contentPadding: EdgeInsets.all(8),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  onSaved: (newValue) {
+                    _password = newValue;
+                  },
+                  validator: (value) {
+                    if (value.length < 8) {
+                      return "Lütfen 8 den fazla karakter gir";
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Password enter",
+
+                    //contentPadding: EdgeInsets.all(8),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      bool isValid = _formKey.currentState.validate();
+                      if (isValid) {
+                        _formKey.currentState.save();
+                        String result =
+                            "user_name: $_userName \n email: $_email \n password:$_password";
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(result),
+                        ));
+                      }
+
+                      _formKey.currentState.reset();
+                    },
+                    child: Text("Submit Form"))
               ],
             ),
           ),
@@ -30,17 +120,24 @@ class _TextFormFieldAppState extends State<TextFormFieldApp> {
     );
   }
 
-  Padding buildTextFormField(label, hintText) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextFormField(
-        decoration: InputDecoration(
-          labelText: label,
-          hintText: hintText,
-          //contentPadding: EdgeInsets.all(8),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(6.0)),
-          ),
+  TextFormField buildTextFormField(label, hintText, isValid) {
+    return TextFormField(
+      validator: (value) {
+        if (isValid) {
+          if (value.length < 4) {
+            return "name length is four charcters";
+          } else {
+            return null;
+          }
+        }
+      },
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hintText,
+
+        //contentPadding: EdgeInsets.all(8),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(6.0)),
         ),
       ),
     );
